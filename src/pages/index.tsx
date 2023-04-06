904ba1456a66e65f8531578dbc0b473ecdb4962f
lisalootens <lisa.lootens@ordina.nl>
2023-04-06 23:48:14 +0200

    Make website mobile-friendly

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+
 import Socials from "../components/socials";
 
 const Home: NextPage = () => {
@@ -12,20 +13,29 @@ const Home: NextPage = () => {
         <link rel="stylesheet" href="/globals.css" />
       </Head>
       <main className={styles.main}>
-        <video
-          src="videos/main_video.mp4"
-          autoPlay
-          loop
-          muted
-          className={styles.backgroundVideo}
-        />
-        <h1 className={styles.title}>
-          Hi, I am <span className={styles.span}>Lisa Lootens</span>.
-        </h1>
-        <p className={styles.description}>
-          Learning to become a developer! I am currently focussing on Java.
-          Recently started as a Software Engineer at Ordina.
-        </p>
+          <section className={styles.circles}>
+              <ul className={styles.circles}>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+              </ul>
+          </section>
+          <section>
+            <h1 className={styles.title}>
+              Hi, I am <span className={styles.span}>Lisa Lootens</span>.
+            </h1>
+            <p className={styles.description}>
+              Learning to become a developer! I am currently focussing on Java.
+              Recently started as a Software Engineer at Ordina.
+            </p>
+          </section>
         <Socials />
       </main>
     </>
