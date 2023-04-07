9c9976e77437d19ad485f4fa2dfb0831039ee45f
lisalootens <lisa.lootens@ordina.nl>
2023-04-07 11:55:47 +0200

    Create separate background component

---
+++ src/components/background.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "../styles/Home.module.css";
+
+export default function Socials(): JSX.Element {
+    return (
+        <section className={styles.circles}>
+            <ul className={styles.circles}>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </section>
+    );
+}
