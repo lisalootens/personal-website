f10685fb1bb562870889ea579b3c3078bf190cb6
lisalootens <lisa.lootens@ordina.nl>
2023-05-23 18:16:36 +0200

    Add titles to photos in carousel

--- src/components/photo-blog/PhotographyMenu.tsx
+++ src/components/photo-blog/PhotographyMenu.tsx
@@ -1,18 +1,21 @@
 import { CarouselSlider } from "../CarouselSlider";
 
 export default function PhotographyMenu() {
-  const menuPhotos: { name: string; src: string }[] = [
+  const menuPhotos: { name: string; src: string; title?: string }[] = [
     {
       name: "dune",
       src: "images/dune.jpg",
+      title: "Africa",
     },
     {
       name: "oryx",
       src: "images/oryx.jpg",
+      title: "Asia",
     },
     {
       name: "zebra",
       src: "images/zebra.jpg",
+      title: "Europe",
     },
   ];
   return <CarouselSlider photos={menuPhotos} />;
