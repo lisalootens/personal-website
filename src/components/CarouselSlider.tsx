7ef6ecf4c491af5535b1d603d41d94be593f7bc4
lisalootens <lisa.lootens@ordina.nl>
2023-05-23 18:16:36 +0200

    Add titles to photos in carousel

--- src/components/CarouselSlider.tsx
+++ src/components/CarouselSlider.tsx
@@ -5,6 +5,7 @@ import { createGlobalStyle } from "styled-components";
 interface Photo {
   name: string;
   src: string;
+  title?: string;
 }
 
 interface CarouselSliderProps {
@@ -15,10 +16,34 @@ const CarouselStyle = createGlobalStyle`
   .keen-slider__slide {
     min-width: 100%;
     height: 80vh;
+    position: relative;
+    overflow: hidden;
+
+    @media (max-width: 418px) {
+      height: 100vh;
+    }
+
+  }
+
+  .title {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: white;
+    font-size: 8vw;
+    font-weight: bold;
+
+    @media (max-width: 960px) {
+      font-size: 15vw;
+    }
   }
 
   img {
     object-fit: cover;
+    object-position: center;
+    width: 100%;
+    height: 100%;
   }
 `;
 
@@ -55,14 +80,12 @@ export const CarouselSlider: React.FC<CarouselSliderProps> = ({ photos }) => {
   return (
     <>
       <CarouselStyle />
-      <div ref={sliderRef} className={"keen-slider"}>
+      <div ref={sliderRef} className="keen-slider">
         {photos.map((photo) => (
-          <img
-            key={photo.name}
-            src={photo.src}
-            alt={photo.name}
-            className="keen-slider__slide"
-          />
+          <div key={photo.name} className="keen-slider__slide">
+            <img key={photo.name} src={photo.src} alt={photo.name} />
+            <div className="title">{photo.title}</div>
+          </div>
         ))}
       </div>
     </>
