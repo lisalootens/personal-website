297daf38a3b4f1438e1b73dc1b43a6e9e5cc7b06
lisalootens <lisa_lootens@live.nl>
2023-05-28 14:00:31 +0200

    Create subpage components for photo gallery

---
+++ src/components/PhotoGallery/Header.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export default function PhotoGalleryHeader(title: string) {
+  const Title = styled.h1`
+    background-color: white;
+    color: #3d3d3d;
+    font-size: 5vw;
+    text-align: center;
+    font-family: "Roboto", sans-serif;
+    letter-spacing: 0.5rem;
+    border-bottom: 1.5px solid lightgrey;
+    padding: 1rem 0;
+    margin: 0 1rem;
+    position: sticky;
+    top: 0;
+  `;
+
+  return (
+    <>
+      <Title>{title}</Title>
+    </>
+  );
+}
