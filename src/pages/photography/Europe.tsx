82adf792061a14c019ad03668f00cfe86f6aa57b
lisalootens <lisa_lootens@live.nl>
2023-05-28 14:02:20 +0200

    Create subpages for photography page

--- src/pages/photography/Europe.tsx
+++ src/pages/photography/Europe.tsx
@@ -3,14 +3,13 @@ import PhotoGalleryHeader from "../../components/PhotoGallery/Header";
 
 export default function AfricaPhotoGallery() {
   const photos = [
-    { name: "Photo dune", src: "/images/dune.jpg" },
     { name: "Photo oryx", src: "/images/oryx.jpg" },
     { name: "Photo zebra", src: "/images/zebra.jpg" },
   ];
 
   return (
     <>
-      {PhotoGalleryHeader("Africa")}
+      {PhotoGalleryHeader("Europe")}
       <PhotoGallery photos={photos} />
     </>
   );
