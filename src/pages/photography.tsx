4806eb1fd0828b29d47400daa4de3d3b1d6349e0
lisalootens <lisa.lootens@ordina.nl>
2023-10-30 17:17:27 +0100

    Create photography menu slider

---
+++ src/pages/photography.tsx
@@ -0,0 +1,45 @@
+import { CarouselSlider } from "../components/CarouselSlider";
+import { createGlobalStyle } from "styled-components";
+import Head from "next/head";
+
+const menuPhotos = [
+  {
+    name: "dune",
+    src: "images/dune.jpg",
+    title: "africa",
+    alt: "Photo of a dune",
+  },
+  {
+    name: "oryx",
+    src: "images/oryx.jpg",
+    title: "asia",
+    alt: "Photo of an oryx",
+  },
+  {
+    name: "zebra",
+    src: "images/zebra.jpg",
+    title: "europe",
+    alt: "Photo of a zebra",
+  },
+];
+
+const PageStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    overflow: hidden;
+  }
+`;
+
+const Photography = () => {
+  return (
+    <>
+      <PageStyle />
+      <Head>
+        <title>Photography</title>
+      </Head>
+      <CarouselSlider photos={menuPhotos} clickable={true} />
+    </>
+  );
+};
+
+export default Photography;
