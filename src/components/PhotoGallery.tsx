b9e0a06384a99f0a3cf5e1ed90c90568db62d625
lisalootens <lisa.lootens@ordina.nl>
2023-10-31 15:37:20 +0100

    Create photo gallery component

---
+++ src/components/PhotoGallery.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { PhotoInterface } from "../interfaces/PhotoInterface";
+import styled from "styled-components";
+
+interface PhotoGalleryProps {
+  photos: PhotoInterface[];
+  title?: string;
+  description?: string;
+}
+
+export const PhotoGallery: React.FC<PhotoGalleryProps> = ({
+  photos,
+  title,
+  description,
+}) => {
+  return (
+    <>
+      <GalleryContainer>
+        {title && description && (
+          <GalleryTitleBlock>
+            <h1>{title}</h1>
+            <p>{description}</p>
+          </GalleryTitleBlock>
+        )}
+        {photos.map((photo) => (
+          <GalleryItem key={photo.src}>
+            <img src={photo.src} alt={photo.name} />
+          </GalleryItem>
+        ))}
+      </GalleryContainer>
+    </>
+  );
+};
+
+const GalleryTitleBlock = styled.section`
+  grid-column: span 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  color: white;
+  text-align: center;
+  background-color: #2a2a2a;
+  padding: 1rem;
+  border-radius: 0.5rem;
+
+  h1 {
+    font-size: 2rem;
+    text-transform: uppercase;
+    letter-spacing: 5px;
+  }
+
+  p {
+    margin: 1rem;
+    color: lightgray;
+  }
+`;
+
+const GalleryContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(30rem, 1fr));
+  gap: 0.5rem;
+`;
+
+const GalleryItem = styled.div`
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 0.5rem;
+`;
