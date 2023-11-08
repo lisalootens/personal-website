081322629be7de5cd2c08dc45d40821d675320e7
lisalootens <lisa.lootens@ordina.nl>
2023-11-08 08:46:21 +0100

    Add metadata (title) for photography pages

---
+++ src/app/photography/gallery/gallery.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { PhotoGallery } from "../../../components/PhotoGallery";
+import { createGlobalStyle } from "styled-components";
+import { getDocs, QueryDocumentSnapshot } from "firebase/firestore";
+import { photoCollection, storage } from "../../../config/firebaseConfig";
+import { getDownloadURL, ref } from "firebase/storage";
+import { useEffect, useState } from "react";
+import { Photo } from "../../../types/Photo";
+import { useSearchParams } from "next/navigation";
+import { BackButton } from "../../../components/buttons/BackButton";
+import { ScrollToTopButton } from "../../../components/buttons/ScrollToTopButton";
+
+async function mapPhotoDocumentToPhoto(
+  document: QueryDocumentSnapshot,
+): Promise<Photo> {
+  const { description, name, location } = document.data();
+  let { src } = document.data();
+  let imageUrl: string;
+  const imageReference = ref(storage, src);
+
+  try {
+    imageUrl = await getDownloadURL(imageReference);
+  } catch (error) {
+    console.log(`Skipped photo ${name}: ${src} not found.`);
+    imageUrl = "skipped";
+    return { name, description, location, src: imageUrl };
+  }
+
+  return { name, description, location, src: imageUrl };
+}
+
+async function getFirestoreData(collection: string) {
+  const documents = await getDocs(photoCollection(collection));
+  const fetchImages = documents.docs.map(mapPhotoDocumentToPhoto);
+  const retrievedImages = await Promise.all(fetchImages);
+
+  return retrievedImages.filter((photo) => photo.src !== "skipped") as Photo[];
+}
+
+export default function Gallery() {
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  const [collection, setCollection] = useState("");
+  const name = useSearchParams().get("name");
+
+  if (!name) {
+    throw new Error("Oops, something went wrong!");
+  }
+
+  useEffect(() => {
+    setCollection(name);
+    getFirestoreData(name).then((data) => setPhotos(data));
+  }, [name, collection]);
+
+  return (
+    <>
+      <PageStyle />
+      <BackButton />
+      <ScrollToTopButton />
+      <PhotoGallery
+        photos={photos}
+        title={name}
+        description={
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, cum cumque debitis dolorem ducimus fuga harum hic nisi optio pariatur perferendis perspiciatis placeat, possimus provident repellendus sapiente temporibus vero vitae."
+        }
+      />
+    </>
+  );
+}
+
+const PageStyle = createGlobalStyle`
+  body {
+    margin: 1rem;
+    background: #1f1f1f;
+  }
+`;
+
--- src/app/photography/page.tsx
+++ src/app/photography/page.tsx
@@ -1,64 +1,10 @@
-"use client";
+import { Metadata } from "next";
+import PhotographyPage from "./photography";
 
-import Head from "next/head";
-import { CarouselSlider } from "../../components/CarouselSlider";
-import { createGlobalStyle } from "styled-components";
-import { useRouter } from "next/navigation";
+export const metadata: Metadata = {
+  title: "Photography",
+};
 
-export default function PhotographyPage() {
-  const router = useRouter();
-  const menuPhotos = [
-    {
-      name: "africa",
-      src: "images/africa.jpg",
-      location: "namibia",
-      description: "Photo of a elephant",
-    },
-    {
-      name: "asia",
-      src: "images/asia.jpg",
-      location: "india",
-      description: "Photo of a mosque",
-    },
-    {
-      name: "europe",
-      src: "images/europe.jpg",
-      location: "the netherlands",
-      description: "Photo of a tree at a foggy sunrise",
-    },
-  ];
-
-  function handleImageClick(event: React.MouseEvent<HTMLElement>) {
-    const name = (event.currentTarget as HTMLImageElement).getAttribute(
-      "data-title",
-    );
-
-    if (name == null) {
-      throw new Error("Unavailable.");
-    }
-    return router.push(`photography/gallery?name=${encodeURIComponent(name)}`);
-  }
-
-  return (
-    <>
-      <PageStyle />
-      <Head>
-        <title>Photography</title>
-      </Head>
-      <CarouselSlider
-        photos={menuPhotos}
-        showDurationBar={true}
-        showTitle={true}
-        clickable={true}
-        handleOnClick={handleImageClick}
-      />
-    </>
-  );
+export default function Page() {
+  return <PhotographyPage />;
 }
-
-const PageStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    overflow: hidden;
-  }
-`;
