262c78c5f7d28bcc02215dd4de203ae0a292d350
lisalootens <lisa.lootens@ordina.nl>
2023-11-09 10:21:19 +0100

    Correct texts

--- public/texts/descriptions.tsx
+++ public/texts/descriptions.tsx
@@ -3,7 +3,7 @@ export const descriptionAfrica: string =
   "Explore the diverse and captivating landscapes, cultures, and wildlife from North to South Africa.";
 
 export const descriptionEurope: string =
-  "Embark on a visual journey through the enchanting beauty and rich traditions of Asia.";
+  "Experience the timeless charm and architectural wonders of Europe.";
 
 export const descriptionAsia: string =
-  "Experience the timeless charm and architectural wonders of Europe.";
+  "Embark on a visual journey through the enchanting beauty and rich traditions of Asia.";
