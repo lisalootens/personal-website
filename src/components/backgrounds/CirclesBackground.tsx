9c84bead9c0b6f3afadb185965d4397ed6c03333
lisalootens <lisa.lootens@ordina.nl>
2023-11-14 14:24:25 +0100

    Refactor: replace styled-components with plain css in background component

---
+++ src/components/backgrounds/CirclesBackground.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./CirclesBackground.css";
+
+export function CirclesBackground(): JSX.Element {
+  return (
+    <div>
+      <ul className={"circles"}>
+        {Array.from({ length: 10 }).map((_, index) => (
+          <li key={`backgroundCircle-${index}`}></li>
+        ))}
+      </ul>
+    </div>
+  );
+}
