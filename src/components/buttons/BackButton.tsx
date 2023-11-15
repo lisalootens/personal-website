8eb195d08f74c6922dcf7fe7d46254e4c9afa3ff
lisalootens <lisa.lootens@ordina.nl>
2023-11-15 13:26:04 +0100

    Replace styled-components with plain CSS and Tailwind CSS for buttons

--- src/components/buttons/BackButton.tsx
+++ src/components/buttons/BackButton.tsx
@@ -1,13 +1,16 @@
 import { useRouter } from "next/navigation";
 import { Arrows } from "../../types/Arrows";
-import "./BackButton.css";
+import "./Buttons.css";
 
 export const BackButton = () => {
   const router = useRouter();
 
   return (
     <div tabIndex={-1}>
-      <button className={"back-button"} onClick={() => router.back()}>
+      <button
+        className={"default-button fill-transparent absolute"}
+        onClick={() => router.back()}
+      >
         <svg height="60" width="60" xmlns="http://www.w3.org/2000/svg">
           <circle className="shape" cx="30" cy="30" r="20" />
           <text x="50%" y="50%" textAnchor="middle" alignmentBaseline="middle">
