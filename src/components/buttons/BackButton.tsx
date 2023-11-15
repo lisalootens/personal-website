b337341dacae517c25401d2be32ea57cb216d2c7
lisalootens <lisa.lootens@ordina.nl>
2023-11-15 11:40:46 +0100

    Replace styled-components with regular CSS for back button

--- src/components/buttons/BackButton.tsx
+++ src/components/buttons/BackButton.tsx
@@ -1,20 +1,20 @@
-import styled from "styled-components";
 import { useRouter } from "next/navigation";
 import { Arrows } from "../../types/Arrows";
+import "./BackButton.css";
 
 export const BackButton = () => {
   const router = useRouter();
 
   return (
     <div tabIndex={-1}>
-      <SvgButton onClick={() => router.back()}>
+      <button className={"back-button"} onClick={() => router.back()}>
         <svg height="60" width="60" xmlns="http://www.w3.org/2000/svg">
           <circle className="shape" cx="30" cy="30" r="20" />
           <text x="50%" y="50%" textAnchor="middle" alignmentBaseline="middle">
             {Arrows.left}
           </text>
         </svg>
-      </SvgButton>
+      </button>
       <style jsx>{`
         .shape:focus {
           outline: -webkit-focus-ring-color auto 0;
@@ -24,33 +24,3 @@ export const BackButton = () => {
   );
 };
 
-const SvgButton = styled.button`
-  position: absolute;
-  background: none;
-  border: none;
-
-  .shape {
-    stroke-dasharray: 140 540;
-    stroke-dashoffset: -474;
-    stroke-width: 2px;
-    fill: transparent;
-    stroke: #19f6e8;
-    transition:
-      stroke-width 0.5s,
-      stroke-dashoffset 0.5s,
-      stroke-dasharray 0.5s;
-  }
-
-  text {
-    cursor: pointer;
-    font-size: 1rem;
-    z-index: 1;
-    fill: #c0c0c0;
-  }
-
-  svg:hover .shape {
-    stroke-width: 1px;
-    stroke-dashoffset: 0;
-    stroke-dasharray: 760;
-  }
-`;
