c4e6fb49b82fcb006993e481de74962684b52f95
lisalootens <lisa.lootens@ordina.nl>
2023-11-20 11:58:21 +0100

    Use server instead of client for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
-import HomePage from "./home-page";
-import { Metadata } from "next";
+import { CirclesBackground } from "../components/backgrounds/CirclesBackground";
+import { Socials } from "../components/Socials";
+import {Metadata} from "next";
 
 export const metadata: Metadata = {
-  title: "Lisa Lootens",
+    title: "Lisa Lootens",
 };
 
 export default function Page() {
-  return <HomePage />;
+  return (
+    <>
+      <CirclesBackground />
+      <main>
+        <section
+          className={
+            "flex flex-col items-center justify-center text-center min-h-screen m-4"
+          }
+        >
+          <h1 className={"text-gray-200 text-4xl font-bold md:text-7xl"}>
+            Hi, I am <span className={"text-amber-600"}>Lisa Lootens</span>.
+          </h1>
+          <p className={"text-gray-200 m-4 text-base md:text-xl"}>
+            Learning to become a developer! I am currently focussing on Java.
+            Recently started as a Software Engineer at Ordina.
+          </p>
+          <Socials />
+        </section>
+      </main>
+    </>
+  );
 }
