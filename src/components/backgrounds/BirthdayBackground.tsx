9cf0d9edeb9be103cbf3be79fbc0143775bdd4e3
Lisa Lootens <lisa_lootens@live.nl>
2023-11-29 08:18:46 +0100

    Create birthday themed balloons for background

---
+++ src/components/backgrounds/BirthdayBackground.tsx
@@ -0,0 +1,20 @@
+import React, { ReactElement } from "react";
+import "./BirthdayBackground.css";
+
+export function BirthdayBackground(): ReactElement {
+  return (
+    <section className={"text-center mt-4 w-full absolute"}>
+      <ul>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <li
+            className={"balloon max-md:opacity-30"}
+            key={`balloon-${index}`}
+          ></li>
+        ))}
+      </ul>
+      <p className={"text-gray-200 tracking-widest"}>
+        My birthday is just around the corner
+      </p>
+    </section>
+  );
+}
