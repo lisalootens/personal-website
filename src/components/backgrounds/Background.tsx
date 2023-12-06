72a421be22202b772d0da3ee16ced768cf4068cf
Lisa Lootens <lisa_lootens@live.nl>
2023-12-06 17:36:42 +0100

    Implement snow background component

--- src/components/backgrounds/Background.tsx
+++ src/components/backgrounds/Background.tsx
@@ -1,13 +1,28 @@
 import { BirthdayBackground } from "./BirthdayBackground";
 import { CirclesBackground } from "./CirclesBackground";
+import { SnowBackground } from "./SnowBackground";
 
 const currentDate = new Date();
 const currentMonth = currentDate.getMonth() + 1;
 const currentDay = currentDate.getDate();
 
-// Every year between the first and 13th of December, show a birthday background
 const isBirthdayBackgroundInRange =
+  // Every year between the first and 13th of December, show a birthday background
   currentMonth === 12 && currentDay >= 1 && currentDay < 13;
 
-export const Background = () =>
-  isBirthdayBackgroundInRange ? <BirthdayBackground /> : <CirclesBackground />;
+const isSnowBackgroundInRange =
+  // When birthday background is not active, show a snowy background during winter time
+  isBirthdayBackgroundInRange
+    ? false
+    : currentMonth === 12 || currentMonth === 1;
+
+export function Background() {
+  switch (true) {
+    case isBirthdayBackgroundInRange:
+      return <BirthdayBackground />;
+    case isSnowBackgroundInRange:
+      return <SnowBackground />;
+    default:
+      return <CirclesBackground />;
+  }
+}
