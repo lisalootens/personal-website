7ed44b5f613dc4eae9ac4d307ba59eee4dc843f8
Lisa Lootens <lisa_lootens@live.nl>
2023-12-06 17:46:15 +0100

    Change date range of birthday background

--- src/components/backgrounds/Background.tsx
+++ src/components/backgrounds/Background.tsx
@@ -8,7 +8,7 @@ const currentDay = currentDate.getDate();
 
 const isBirthdayBackgroundInRange =
   // Every year between the first and 13th of December, show a birthday background
-  currentMonth === 12 && currentDay >= 1 && currentDay < 13;
+  currentMonth === 12 && currentDay > 6 && currentDay < 13;
 
 const isSnowBackgroundInRange =
   // When birthday background is not active, show a snowy background during winter time
